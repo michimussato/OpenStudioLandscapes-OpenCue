959f295fad11c036e7d77c47b99c6e49fbfc8210
Michael Mussato <michimussato@gmail.com>
2026-02-15 23:12:21 +0100

    - moves tz to engine

--- src/OpenStudioLandscapes/OpenCue/assets.py
+++ src/OpenStudioLandscapes/OpenCue/assets.py
@@ -375,7 +375,7 @@ def compose_cuebot(
                 "domainname": config_engine.openstudiolandscapes__domain_lan,
                 "restart": DockerComposePolicies.RESTART_POLICY.ALWAYS.value,
                 "environment": {
-                    "TZ": CONFIG.tz,
+                    "TZ": config_engine.tz,
                     "CUE_FRAME_LOG_DIR": "/tmp/rqd/logs",
                     **config_engine.global_environment_variables,
                     **CONFIG.local_environment_variables,
@@ -548,7 +548,7 @@ def compose_flyway(
                 "domainname": config_engine.openstudiolandscapes__domain_lan,
                 "restart": DockerComposePolicies.RESTART_POLICY.NO.value,
                 "environment": {
-                    "TZ": CONFIG.tz,
+                    "TZ": config_engine.tz,
                     "PGHOST": container_name_db,
                     "PGDATABASE": CONFIG.OPENCUE_DB_PGDATABASE,
                     "PGPASSWORD": CONFIG.OPENCUE_DB_PGPASSWORD,
@@ -710,7 +710,7 @@ def compose_db(
                 "domainname": config_engine.openstudiolandscapes__domain_lan,
                 "restart": DockerComposePolicies.RESTART_POLICY.ALWAYS.value,
                 "environment": {
-                    "TZ": CONFIG.tz,
+                    "TZ": config_engine.tz,
                     "POSTGRES_USER": CONFIG.OPENCUE_DB_PGUSER,
                     "POSTGRES_PASSWORD": CONFIG.OPENCUE_DB_PGPASSWORD,
                     "POSTGRES_DB": CONFIG.OPENCUE_DB_PGDATABASE,
@@ -866,7 +866,7 @@ def compose_rqd(
                     "domainname": config_engine.openstudiolandscapes__domain_lan,
                     "restart": DockerComposePolicies.RESTART_POLICY.ALWAYS.value,
                     "environment": {
-                        "TZ": CONFIG.tz,
+                        "TZ": config_engine.tz,
                         "PYTHONUNBUFFERED": "1",
                         # Todo:
                         #  - [ ] use fqdn instead of just hostname?
@@ -1035,7 +1035,7 @@ def compose_rest_gateway(
                 },
                 "domainname": config_engine.openstudiolandscapes__domain_lan,
                 "environment": {
-                    "TZ": CONFIG.tz,
+                    "TZ": config_engine.tz,
                     # https://docs.opencue.io/docs/getting-started/deploying-rest-gateway/#configuration-options
                     "CUEBOT_ENDPOINT": f"{container_name_cuebot}:{CONFIG.OPENCUE_CUEBOT_GRPC_CUE_PORT_CONTAINER}",
                     "REST_PORT": CONFIG.OPENCUE_REST_GATEWAY_PORT_CONTAINER,
@@ -1229,7 +1229,7 @@ def compose_cueweb(
                 },
                 "domainname": config_engine.openstudiolandscapes__domain_lan,
                 "environment": {
-                    "TZ": CONFIG.tz,
+                    "TZ": config_engine.tz,
                     # https://docs.opencue.io/docs/getting-started/deploying-cueweb/#environment-configuration
                     "NEXT_PUBLIC_OPENCUE_ENDPOINT": f"http://{host_name_rest_gateway}:{CONFIG.OPENCUE_REST_GATEWAY_PORT_CONTAINER}",
                     "NEXT_PUBLIC_URL": f"http://{host_name_cueweb}:{CONFIG.OPENCUE_CUEWEB_PORT_HOST}",
