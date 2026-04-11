9e2842191381aa848e86fcd3da75a1a96826fb0d
Michael Mussato <michimussato@gmail.com>
2026-04-11 13:30:24 +0200

    implements individual development/testing/debugging functionality

--- src/OpenStudioLandscapes/OpenCue/definitions.py
+++ src/OpenStudioLandscapes/OpenCue/definitions.py
@@ -4,16 +4,14 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.OpenCue.assets
-from OpenStudioLandscapes.engine.features.upstream_asset_specs import assets_external
 
-assets = load_assets_from_modules(
+assets_base = load_assets_from_modules(
     modules=[OpenStudioLandscapes.OpenCue.assets],
 )
 
 
 defs = Definitions(
     assets=[
-        *assets,
-        *assets_external,
+        *assets_base,
     ],
 )
