41b868452793c635451c4640970d5dd94bcace48
Michael Mussato <michimussato@gmail.com>
2026-04-26 17:52:02 +0200

    add computed_field to properties

--- src/OpenStudioLandscapes/OpenCue/config/models.py
+++ src/OpenStudioLandscapes/OpenCue/config/models.py
@@ -7,6 +7,7 @@ from pydantic import (
     Field,
     HttpUrl,
     PositiveInt,
+    computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -250,6 +251,7 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
+    @computed_field
     @property
     def OPENCUE_DB_INSTALL_DESTINATION_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
